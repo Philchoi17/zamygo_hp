6a76b00ead8ac680bd30f8c9bc90feeb16d76cb0
PhilChoi <philchoi@icloud.com>
2020-09-06 13:57:06 +0900

    push build again

---
+++ build/precache-manifest.3f44026da7aff984518d5457c3cb160d.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73bd585367d523fa5a800905f5cc9e76",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5ed3e7b4ca0090314db9",
+    "url": "./static/css/main.a531f607.chunk.css"
+  },
+  {
+    "revision": "97ed7598ebaefbe2b4e4",
+    "url": "./static/js/2.cafc9a53.chunk.js"
+  },
+  {
+    "revision": "d69f22d91a8936d2b44adcdf9ec3bd30",
+    "url": "./static/js/2.cafc9a53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ed3e7b4ca0090314db9",
+    "url": "./static/js/main.17335feb.chunk.js"
+  },
+  {
+    "revision": "30f942bb7783c4b076a0017ec0876fab",
+    "url": "./static/js/main.17335feb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3da7a761483cf3ecc8e9",
+    "url": "./static/js/runtime-main.943d2efd.js"
+  },
+  {
+    "revision": "855ef5942c270ffc4e7631117bcf8d7b",
+    "url": "./static/media/DK_Feature.855ef594.png"
+  },
+  {
+    "revision": "dbe893c682bc16278c50cd0100108e24",
+    "url": "./static/media/DK_Poems.dbe893c6.png"
+  },
+  {
+    "revision": "4f0030a88afa428ea823007bdc9388e2",
+    "url": "./static/media/DK_Sculpture.4f0030a8.png"
+  },
+  {
+    "revision": "1b53b7c8bd5cb4bdcb641423f0cd16db",
+    "url": "./static/media/DK_Short.1b53b7c8.png"
+  },
+  {
+    "revision": "d259e267669c3366c66d650e40860495",
+    "url": "./static/media/Profile_Pic.d259e267.jpg"
+  },
+  {
+    "revision": "f855f46d6843c9bca56ebcd35cccbd5b",
+    "url": "./static/media/TKH_Header.f855f46d.jpg"
+  },
+  {
+    "revision": "8426e92aa34c3ea476cad0491ff5a3ca",
+    "url": "./static/media/Tonko_House_Logo.8426e92a.png"
+  },
+  {
+    "revision": "e525b5d8713f6633b433cafc81533763",
+    "url": "./static/media/damkeeperpress.e525b5d8.png"
+  },
+  {
+    "revision": "cadc60b9d7724c6648fd7fa40cf3ec91",
+    "url": "./static/media/ewpress.cadc60b9.png"
+  },
+  {
+    "revision": "21657ccb68798f32d120f12157d28fc3",
+    "url": "./static/media/hollywoodpress.21657ccb.png"
+  },
+  {
+    "revision": "e7a794147742e0071f587438b06b3fa8",
+    "url": "./static/media/monaclepress.e7a79414.png"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "./static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "./static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "./static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "./static/media/nucleo-icons.f32ed5ad.eot"
+  },
+  {
+    "revision": "e00a6c9f941a516400b5e7bc82c2ee7b",
+    "url": "./static/media/nytimespress.e00a6c9f.png"
+  },
+  {
+    "revision": "46ce72e000e748a84fe207941cba0b70",
+    "url": "./static/media/pigseries1.46ce72e0.jpg"
+  },
+  {
+    "revision": "a025945760037990abff6ea206dffd05",
+    "url": "./static/media/pigseries2.a0259457.jpg"
+  },
+  {
+    "revision": "2355b5f693f48ddaa32194ec8d163a1d",
+    "url": "./static/media/pigseries3.2355b5f6.jpg"
+  },
+  {
+    "revision": "f855f46d6843c9bca56ebcd35cccbd5b",
+    "url": "./static/media/thedamkeeper.f855f46d.jpg"
+  },
+  {
+    "revision": "ca5d7d778ea2ebcaf9139fa5d28d7fb6",
+    "url": "./static/media/thedamkeeper1.ca5d7d77.jpg"
+  },
+  {
+    "revision": "c80ace86ebb885c884139ee27e54a491",
+    "url": "./static/media/thedamkeeper2.c80ace86.jpg"
+  },
+  {
+    "revision": "6116dcbd9596e10a2e351afb5e7a544c",
+    "url": "./static/media/thedamkeeper3.6116dcbd.jpg"
+  },
+  {
+    "revision": "c72e3b56046cead37c37b21ac994629d",
+    "url": "./static/media/thedamkeeper4.c72e3b56.png"
+  },
+  {
+    "revision": "bc95f2ec1ee7aeca8ee60eb8ae8f1f52",
+    "url": "./static/media/varietypress.bc95f2ec.png"
+  },
+  {
+    "revision": "61e909df36bdba6fe95dd1f548a16673",
+    "url": "./static/media/zamygologo.61e909df.png"
+  }
+]);
